f51e144a89920333e8739c9427e83974e18fb33c
kohei-tofu <fukouhei001@gmail.com>
2021-11-26 22:13:47 +0900

    update to react-router v6

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,5 +1,9 @@
 import React, { useContext, useState, useEffect } from "react"
 import { auth } from "../firebase"
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, 
+         signOut, onAuthStateChanged, updateEmail, updatePassword, sendPasswordResetEmail,
+        } from "firebase/auth";
+
 
 const AuthContext = React.createContext()
 
@@ -12,31 +16,31 @@ export function AuthProvider({ children }) {
   const [loading, setLoading] = useState(true)
 
   function signup(email, password) {
-    return auth.createUserWithEmailAndPassword(email, password)
+    return createUserWithEmailAndPassword(auth, email, password)
   }
 
   function login(email, password) {
-    return auth.signInWithEmailAndPassword(email, password)
+    return signInWithEmailAndPassword(auth, email, password)
   }
 
   function logout() {
-    return auth.signOut()
+    return signOut(auth)
   }
 
   function resetPassword(email) {
-    return auth.sendPasswordResetEmail(email)
+    return sendPasswordResetEmail(auth, email)
   }
 
   function updateEmail(email) {
-    return currentUser.updateEmail(email)
+    return updateEmail(currentUser, email)
   }
 
   function updatePassword(password) {
-    return currentUser.updatePassword(password)
+    return updatePassword(currentUser, password)
   }
-
+  
   useEffect(() => {
-    const unsubscribe = auth.onAuthStateChanged(user => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
       setCurrentUser(user)
       setLoading(false)
     })
